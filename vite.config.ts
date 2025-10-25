5cd56ce3015c8f461412c807f87e3715c3e76cc8
akutuva21 <akutuva22@gmail.com>
2025-10-25 16:00:35 -0400

    Rebuild with base path

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(() => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: '/bnglplayground/',
       plugins: [react()],
       resolve: {
         alias: {
